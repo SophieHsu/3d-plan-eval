dc7c7d689a9d519d4c6572520daf6920216757d0
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-04-15 20:15:16 -0700

    implement the fridge object

--- lsi_3d/environment/objects.py
+++ lsi_3d/environment/objects.py
@@ -5,19 +5,23 @@ from igibson.objects.articulated_object import URDFObject
 class KitchenObject:
     def __init__(self):
         self._params = None
+        self._obj = None
 
-    def create_urdf_object(self):
-        return URDFObject(
-            filename=self._params.filename,
-            avg_obj_dims=self._params.avg_obj_dims,
-            scale=self._params.scale / self._params.scale_factor,
-            model_path=self._params.model_uri,
-            category=self._params.category,
-            fixed_base=self._params.fixed_base,
-        )
+    @property
+    def obj(self):
+        if self._obj is None:
+            self._obj = URDFObject(
+                filename=self._params.filename,
+                avg_obj_dims=self._params.avg_obj_dims,
+                scale=self._params.scale / self._params.scale_factor,
+                model_path=self._params.model_uri,
+                category=self._params.category,
+                fixed_base=self._params.fixed_base,
+            )
+        return self._obj
 
     def load(self, *args):
-        raise NotImplementedError()
+        self._params.obj_handlers.import_obj(self._obj)
 
 
 class Fridge(KitchenObject):
@@ -26,8 +30,6 @@ class Fridge(KitchenObject):
 
         self._params = Namespace(**kwargs)
 
-    def load(self, obj_handlers):
-        obj = self.create_urdf_object()
-        obj_handlers.import_obj(obj)
-        obj_handlers.set_pos_orn(obj, self._params.pos, self._params.orn)
-
+    def load(self):
+        self._params.obj_handlers.import_obj(self._obj)
+        self._params.obj_handlers.set_pos_orn(self._obj, self._params.pos, self._params.orn)
