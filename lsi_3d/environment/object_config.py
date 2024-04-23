1f69f20faa25f06083c6465cddcc8fbc9e6b3d98
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-04-22 21:51:05 -0700

    add object configs for 4 objects (counter, vidalia_onion, steak and plate)

--- lsi_3d/environment/object_config.py
+++ lsi_3d/environment/object_config.py
@@ -37,3 +37,43 @@ CONF_KEYS = SimpleNamespace(
     CATEGORY='category',
     FIXED_BASE='fixed_base',
 )
+
+OBJECT_CONFIG = SimpleNamespace(**{
+    OBJECT_KEYS.COUNTER: {
+        CONF_KEYS.FILENAME: os.path.join(ig_dataset_path, 'objects/bottom_cabinet/46452/46452.urdf'),
+        CONF_KEYS.DENSITY: 10000,
+        CONF_KEYS.SCALE: np.array([1.04, 0.97, 0.95]) / 1.15,
+        CONF_KEYS.MODEL_PATH: os.path.dirname(os.path.join(ig_dataset_path, 'objects/bottom_cabinet/46452/46452.urdf')),
+        CONF_KEYS.CATEGORY: OBJECT_KEYS.COUNTER,
+        CONF_KEYS.FIXED_BASE: True,
+    },
+    OBJECT_KEYS.VIDALIA_ONION: {
+        CONF_KEYS.FILENAME: os.path.join(ig_dataset_path, 'objects/vidalia_onion/18_1/18_1.urdf'),
+        CONF_KEYS.DENSITY: 10000,
+        CONF_KEYS.SCALE: np.array([2.0, 2.0, 2.0]) / 1.15,
+        CONF_KEYS.MODEL_PATH: os.path.dirname(os.path.join(ig_dataset_path, 'objects/vidalia_onion/18_1/18_1.urdf')),
+        CONF_KEYS.CATEGORY: OBJECT_KEYS.VIDALIA_ONION,
+        CONF_KEYS.FIXED_BASE: False,
+    },
+    OBJECT_KEYS.STEAK: {
+        CONF_KEYS.FILENAME: os.path.join(ig_dataset_path, 'objects/steak/steak_000/steak_000.urdf'),
+        CONF_KEYS.DENSITY: 10000,
+        CONF_KEYS.SCALE: np.array([2.0, 2.0, 2.0]) / 1.15,
+        CONF_KEYS.MODEL_PATH: os.path.dirname(os.path.join(ig_dataset_path, 'objects/steak/steak_000/steak_000.urdf')),
+        CONF_KEYS.CATEGORY: OBJECT_KEYS.STEAK,
+        CONF_KEYS.FIXED_BASE: False,
+    },
+    OBJECT_KEYS.PLATE: {
+        CONF_KEYS.FILENAME: os.path.join(
+            ig_dataset_path,
+            'objects/bowl/a1393437aac09108d627bfab5d10d45d/a1393437aac09108d627bfab5d10d45d.urdf'
+        ),
+        CONF_KEYS.SCALE: np.array([2.0, 2.0, 2.0]) / 1.15,
+        CONF_KEYS.MODEL_PATH: os.path.dirname(os.path.join(
+            ig_dataset_path,
+            'objects/bowl/a1393437aac09108d627bfab5d10d45d/a1393437aac09108d627bfab5d10d45d.urdf'
+        )),
+        CONF_KEYS.CATEGORY: OBJECT_KEYS.PLATE,
+        CONF_KEYS.FIXED_BASE: False,
+    },
+})
