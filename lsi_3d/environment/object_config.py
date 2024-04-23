4789189421910c43e95380800db1904d4ff24516
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-04-22 21:46:04 -0700

    update keys to be in lowercase

--- lsi_3d/environment/object_config.py
+++ lsi_3d/environment/object_config.py
@@ -1,28 +1,31 @@
+import os
+import numpy as np
 from types import SimpleNamespace
+from igibson import ig_dataset_path
 
 
 OBJECT_KEYS = SimpleNamespace(
-    COUNTER='COUNTER',
-    TABLE='TABLE',
-    STOVE='STOVE',
-    BOWL='BOWL',
-    COLOR_BOWL='COLOR_BOWL',
-    LARGE_BOWL='LARGE_BOWL',
-    STEAK_BOWL='STEAK_BOWL',
-    PAN='PAN',
-    SINK='SINK',
-    FRIDGE='FRIDGE',
-    VIDALIA_ONION='VIDALIA_ONION',
-    BROCCOLI='BROCCOLI',
-    GREEN_ONION='GREEN_ONION',
-    STEAK='STEAK',
-    TRAY='TRAY',
-    APPLE='APPLE',
-    PLATE='PLATE',
-    SCRUB_BRUSH='SCRUB_BRUSH',
-    CHOPPING_BOARD='CHOPPING_BOARD',
-    KNIFE='KNIFE',
-    ONION='ONION'
+    COUNTER='counter',
+    TABLE='table',
+    STOVE='stove',
+    BOWL='bowl',
+    COLOR_BOWL='color_bowl',
+    LARGE_BOWL='large_bowl',
+    STEAK_BOWL='steak_bowl',
+    PAN='pan',
+    SINK='sink',
+    FRIDGE='fridge',
+    VIDALIA_ONION='vidalia_onion',
+    BROCCOLI='broccoli',
+    GREEN_ONION='green_onion',
+    STEAK='steak',
+    TRAY='tray',
+    APPLE='apple',
+    PLATE='plate',
+    SCRUB_BRUSH='scrub_brush',
+    CHOPPING_BOARD='shopping_board',
+    KNIFE='knife',
+    ONION='onion'
 )
 
 CONF_KEYS = SimpleNamespace(
